1c51be02dab35090ebf4034370087c9fafa7725f
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-12 11:12:49 +0100

    Remove quaternius from playground

--- src/components/quaternius/mech_pack/index.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic'
-
-export const George = dynamic(() => import('./George'), { ssr: false })
-export const Leela = dynamic(() => import('./Leela'), { ssr: false })
-export const Mike = dynamic(() => import('./Mike'), { ssr: false })
-export const Stan = dynamic(() => import('./Stan'), { ssr: false })
--- src/components/quaternius/tank_pack/index.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic'
-
-export const Tank = dynamic(() => import('./Tank'), { ssr: false })
-export const Tank2 = dynamic(() => import('./Tank2'), { ssr: false })
-export const Tank3 = dynamic(() => import('./Tank3'), { ssr: false })
-export const Tank4 = dynamic(() => import('./Tank4'), { ssr: false })
