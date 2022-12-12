b0eda5e1ebf6607e83bbb6f31de5d037f71c81ca
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-12 15:15:37 +0100

    Add simple water effect

--- src/components/canvas/FreeMovement.tsx
+++ src/components/canvas/FreeMovement.tsx
@@ -5,15 +5,24 @@ import { PointerLockControls, Sky } from '@react-three/drei'
 import { Plane } from './ThirdPersonDemo'
 import { Trees } from './TreeStuff'
 
+// color palette underwater
+// #daf8e3
+// #97ebdb
+// #00c2c7
+// #0086ad
+// #005582
+
 export default function FreeMovement() {
   return (
     <>
-      <Sky azimuth={1} inclination={0.6} distance={1000} />
+      {/* <Sky azimuth={1} inclination={0.6} distance={1000} /> */}
 
       <Physics>
         <FlyingPlayer />
       </Physics>
       <PointerLockControls />
+      <fogExp2 attach='fog' color='#0086ad' density={0.03} />
+      <color attach='background' args={['#0086ad']} />
 
       <FollowingTrex />
 
