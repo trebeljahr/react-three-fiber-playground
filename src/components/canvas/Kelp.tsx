e8fb969b98e83059017a3b84ad6e1e53d5444a64
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-14 22:21:47 +0100

    Make kelp forests infinite

--- src/components/canvas/Kelp.tsx
+++ src/components/canvas/Kelp.tsx
@@ -3,7 +3,17 @@ import { useFrame, useThree } from '@react-three/fiber'
 import kelpVert from '@shaders/kelp.vert'
 import FastPoissonDiskSampling from 'fast-2d-poisson-disk-sampling'
 import { useEffect, useMemo, useRef } from 'react'
-import { BufferGeometry, InstancedMesh, MeshPhysicalMaterial, Object3D, ShaderMaterial } from 'three'
+import {
+  BufferGeometry,
+  DynamicDrawUsage,
+  InstancedMesh,
+  Matrix4,
+  MeshPhysicalMaterial,
+  Object3D,
+  Quaternion,
+  ShaderMaterial,
+  Vector3,
+} from 'three'
 import CustomShaderMaterial from 'three-custom-shader-material'
 import CustomShaderMaterialType from 'three-custom-shader-material/vanilla'
 import { randFloat } from 'three/src/math/MathUtils'
@@ -39,8 +49,12 @@ export function CustomKelpShaderMaterial() {
 }
 
 export function KelpForest({ size = 150 }) {
-  const { nodes } = useKelp()
+  const ref = useRef<InstancedMesh<BufferGeometry, ShaderMaterial>>()
 
+  const { nodes } = useKelp()
+  const kelpGeometry = nodes.Object_7.geometry
+
+  // const points = useMemo(() => [[0, 0]], [])
   const points = useMemo(() => {
     const sampler = new FastPoissonDiskSampling({
       shape: [size, size],
@@ -48,23 +62,19 @@ export function KelpForest({ size = 150 }) {
       tries: 10,
     })
     const result = sampler.fill()
-    return result
+    console.log(result.length)
+    return result.map(([x, z]) => [x - size / 2, z - size / 2])
   }, [size])
 
-  const kelpGeometry = nodes.Object_7.geometry
-
-  const ref = useRef<InstancedMesh<BufferGeometry, ShaderMaterial>>()
+  const { camera } = useThree()
 
   useEffect(() => {
     const temp = new Object3D()
 
     for (let i = 0; i < points.length; i++) {
       const scale = randFloat(0.2, 0.3)
-
-      //   temp.position.set(0, 0, 0)
-      // temp.position.set(randFloat(-size, size), 0, randFloat(-size, size))
       const [x, z] = points[i]
-      temp.position.set(x - size / 2, 0, z - size / 2)
+      temp.position.set(x, 0, z)
       temp.rotation.set(0, randFloat(0.2, 0.5), 0)
       temp.scale.set(scale, scale, scale)
 
@@ -73,8 +83,48 @@ export function KelpForest({ size = 150 }) {
       ref.current.setMatrixAt(i, temp.matrix)
     }
 
+    ref.current.instanceMatrix.setUsage(DynamicDrawUsage)
     ref.current.instanceMatrix.needsUpdate = true
   }, [size, points])
+
+  useFrame(() => {
+    if (!ref.current) return
+    for (let i = 0; i < points.length; i++) {
+      const temp = new Object3D()
+
+      const position = new Vector3(0, 0, 0)
+      const scale = new Vector3(0, 0, 0)
+      const rotation = new Quaternion()
+
+      let needsUpdate = false
+
+      let matrix = new Matrix4()
+      ref.current.updateMatrixWorld()
+      ref.current.getMatrixAt(i, matrix)
+
+      matrix.decompose(position, rotation, scale)
+      // console.log(position)
+      temp.matrix = matrix
+
+      const cameraXZPos = camera.position.clone().setY(0)
+      if (Math.abs(position.distanceToSquared(cameraXZPos)) > 14000) {
+        // console.log('too far!')
+        const diffVec = position.sub(cameraXZPos).negate()
+        const { x, z } = diffVec.multiplyScalar(0.99).add(cameraXZPos)
+
+        temp.position.set(x, 0, z)
+        temp.rotation.setFromQuaternion(rotation)
+        temp.scale.set(scale.x, scale.y, scale.z)
+        temp.updateMatrix()
+        ref.current.setMatrixAt(i, temp.matrix)
+        needsUpdate = true
+      }
+
+      // console.log(temp.matrix)
+
+      ref.current.instanceMatrix.needsUpdate = needsUpdate
+    }
+  })
 
   return (
     <instancedMesh ref={ref} args={[kelpGeometry, null, points.length]}>
--- src/shaders/kelp.vert
+++ src/shaders/kelp.vert
@@ -8,8 +8,8 @@ void main() {
 
   // float offset = smoothstep(0.0, 10.0, 20.0);
   // modifiedPosition.x += sin(position.y + uTime);
-  modifiedPosition.x += sin(position.y / 20.0 + uTime * 0.5) * 4.0;
-  modifiedPosition.z += sin(position.y / 10.0 + uTime * 0.5 + 2.5) * 4.0;
+  modifiedPosition.x += sin(position.y / 30.0 + uTime * 0.5) * 4.0;
+  // modifiedPosition.z += sin(position.y / 10.0 + uTime * 0.5 + 2.5) * 4.0;
 
   // modifiedPosition.z += sin(uTime) * offset;
 
