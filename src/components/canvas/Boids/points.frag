f8587bb013a07947d4d7bfa675e69110c05a916a
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-01-04 12:37:37 +0100

    Rename and move things

--- src/components/canvas/Boids/points.frag
+++
@@ -1,4 +0,0 @@
-void main() {
-  vec3 color = vec3(0.96f, 0.59f, 0.34f);
-  gl_FragColor = vec4(color, 1.0);
-}
--- src/pages/birds.tsx
+++ src/pages/birds.tsx
@@ -1,4 +1,4 @@
-import { Birds } from '@components/canvas/Boids/Birds'
+import { Birds } from '@components/canvas/Birds'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
@@ -6,10 +6,8 @@ import { Perf } from 'r3f-perf'
 export default function Page() {
   return (
     <Canvas camera={{ position: [0, 0, 350], near: 1, far: 3000 }}>
-      {/* <ambientLight intensity={0.5} /> */}
       <Birds />
       <fog color={0xffffff} near={100} far={1000} />
-      {/* <OrbitControls /> */}
       <Perf />
     </Canvas>
   )
